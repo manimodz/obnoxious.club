84b62ffb32692b449ae4b43687cc795cc803343a
manimodz <62816306+manimodz@users.noreply.github.com>
2020-03-29 03:09:07 +0100

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,7 +22,7 @@ var _app = function () {
 
     this.titleChanger = function (text, delay) {
         text = text ||
-            ['Lummit', 'Neso', '', 'Vez', '', '=', '', 'Song name: KUSO GVKI - Cinnamon (feat. Apfel) (splash! Mag Premiere)'];
+            ['Lummit', '', '', '', '', '', '', 'Song name: KUSO GVKI - Cinnamon (feat. Apfel) (splash! Mag Premiere)'];
         delay = delay || 2000;
 
         var counter = 0;
